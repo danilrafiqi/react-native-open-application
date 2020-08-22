034b321391bec955b4087099ad376599ae371771
M Danil Rafiqi <danil.rafiqi@gmail.com>
2020-08-22 16:12:52 +0700

    fix promise

--- android/src/main/java/com/reactlibrary/OpenApplicationModule.java
+++ android/src/main/java/com/reactlibrary/OpenApplicationModule.java
@@ -4,6 +4,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 public class OpenApplicationModule extends ReactContextBaseJavaModule {
 
